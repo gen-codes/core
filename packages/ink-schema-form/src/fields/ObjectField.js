529217f5e609d5a9de2ad4c6343bb746e1347db3
Vasilis Kefallinos <vkefallinos@gmail.com>
2020-01-04 17:36:49 +0200

    remove logs

--- packages/ink-schema-form/src/fields/ObjectField.js
+++ packages/ink-schema-form/src/fields/ObjectField.js
@@ -11,21 +11,21 @@ import {handleProperties} from '../utils/handleProperties';
 import {checkCondition} from '../utils/checkCondition';
 import Divider from "../components/Divider"
 import {FormContext} from "../index"
-const winston = require('winston');
+// const winston = require('winston');
 
-const logger = winston.createLogger({
-  level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' })
-  ]
-});
+// const logger = winston.createLogger({
+//   level: 'info',
+//   format: winston.format.json(),
+//   defaultMeta: { service: 'user-service' },
+//   transports: [
+//     //
+//     // - Write all logs with level `error` and below to `error.log`
+//     // - Write all logs with level `info` and below to `combined.log`
+//     //
+//     new winston.transports.File({ filename: 'error.log', level: 'error' }),
+//     new winston.transports.File({ filename: 'combined.log' })
+//   ]
+// });
 
 
 
@@ -39,8 +39,8 @@ export default function ObjectField({objectType, schema, prefix = "", ...props})
   useEffect(() => {
     const newFields = fields.filter((field) => {
       if(field.condition) {
-        logger.info(field.condition);
-        logger.info(formData);
+        // logger.info(field.condition);
+        // logger.info(formData);
         return checkCondition(field.condition, formData)
       }
       return true
