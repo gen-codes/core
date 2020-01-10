c873b243d70037f17263f5076c5b5a59fba6949c
Vasilis Kefallinos <vkefallinos@gmail.com>
2020-01-10 15:59:57 +0200

    add better switch command handling

--- packages/ink-schema-form/src/utils/handleProperties.js
+++ packages/ink-schema-form/src/utils/handleProperties.js
@@ -1,39 +1,31 @@
 import {handleProperty} from './handleProperty';
 import IdInput from '../components/IdInput';
-export function handleProperties(properties, condition, schema, prefix, root=true) {
-	let fields = [];
-	for(const propName in properties) {
+export function handleProperties(properties, condition, schema, prefix, root = true) {
+  let fields = [];
+  for(const propName in properties) {
     const prop = properties[propName];
-    if(propName.startsWith("_switch")){
-      const switches = Object.keys(prop)
-      fields = fields.concat(
-        switches.reduce((props,pName)=>{
-          const values = Object.keys(prop[pName])
-          return props.concat(values.reduce((subprops, v)=>{
-            const condition = `${pName} === '${v}'`
-            return subprops.concat(handleProperties(
-              prop[pName][v], condition, schema,`${prefix}`, false
-            ))
-          },[]))
+    if(propName.startsWith("switch(")) {
+      const switchField = propName.match(/\((.*?)\)/)[1]
+      for(const value in prop) {
+        const condition = `${switchField} === ${value}`
+        fields = fields.concat(handleProperties(prop[value], condition, schema, `${prefix}`, false))
+      }
 
-        },[])
-      )
     }
-		if(propName.startsWith("if(")) {
-			const condition = propName.match(/if\((.*?)\)$/)[1];
-			fields = fields.concat(handleProperties(prop, condition, schema,`${prefix}`, false));
-		}
-		else {
-			fields.push({prefix: `${prefix}.${propName}`,...handleProperty(propName, prop, condition, schema)});
-		}
-	}
-	if(root){
-		fields.push({
-			Input: IdInput,
-			name: "id",
-			label: "id"
+    if(propName.startsWith("if(")) {
+      const condition = propName.match(/if\((.*?)\)$/)[1];
+      fields = fields.concat(handleProperties(prop, condition, schema, `${prefix}`, false));
+    }else {
+      fields.push({prefix: `${prefix}.${propName}`, ...handleProperty(propName, prop, condition, schema)});
+    }
+  }
+  if(root) {
+    fields.push({
+      Input: IdInput,
+      name: "id",
+      label: "id"
 
-		})
-	}
-	return fields;
+    })
+  }
+  return fields;
 }
